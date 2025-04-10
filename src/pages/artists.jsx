48bf229da92074393a06562e8ba59e0c08da0914
pchinovsky <144697881+pchinovsky@users.noreply.github.com>
2025-04-10 20:55:28 +0300

    Positioning update.

--- src/pages/artists.jsx
+++ src/pages/artists.jsx
@@ -23,7 +23,7 @@ export default function ArtistsPage() {
 
     return (
         <DefaultLayout>
-            <div className="p-4">
+            <div className="p-4 mt-20">
                 <h1 className="text-2xl font-bold mb-4">Artists</h1>
                 <div
                     className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 overflow-y-auto"
